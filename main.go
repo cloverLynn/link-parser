586ec6e1faa5252aa9f59136a5201fee2a1bfff3
cForbes <clover.forbes96@gmail.com>
2024-04-11 11:44:45 -0400

    added some TODOs to structure future code

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+//TODO Link Struct
+
+//TODO loadHTML
+func loadHTML(name string) os.File {
+	return os.File{}
+}
+
+//TODO parseHTML
+func parseHTML(f os.File) {
+
+}
+
+//TODO unmarshallHTML
+func unmarshallHTML() {
+
+}
 
 func main() {
 	fmt.Println("TOOD EVERYTHING")
