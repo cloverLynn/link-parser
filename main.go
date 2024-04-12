599f1d3b7a6a81f76389faf10ed61ebff78b1393
cForbes <clover.forbes96@gmail.com>
2024-04-11 23:58:46 -0400

    program is mostly working with all given examples need to clean text being passed to Link.Text

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
 	"os"
+	"strings"
 )
+
+//"golang.org/x/net/html"
 
 type Link struct {
 	Href string
@@ -11,20 +15,44 @@ type Link struct {
 }
 
 // TODO loadHTML
-func loadHTML(name string) os.File {
-	return os.File{}
+func loadHTML(name string) string {
+	file, _ := os.ReadFile(name)
+	return string(file)
 }
 
 // TODO parseHTML
-func parseHTML(f os.File) {
-
+func parseHTML(file string) []Link {
+	doc, err := html.Parse(strings.NewReader(file))
+	if err != nil {
+		fmt.Println("Error parsing HTML:", err)
+		panic("File Error")
+	}
+	var sl []Link
+	buildSlice(doc, 0, &sl)
+	return sl
 }
 
-// TODO unmarshallHTML
-func unmarshallHTML() {
+func buildSlice(n *html.Node, depth int, sl *[]Link) {
+	if n.Type == html.ElementNode {
+		if n.DataAtom.String() == "a" {
+			l := Link{
+				Href: n.Attr[0].Val,
+				Text: n.FirstChild.Data,
+			}
+			*sl = append(*sl, l)
+		}
+	}
 
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		buildSlice(c, depth+1, sl)
+	}
 }
 
 func main() {
-	fmt.Println("TOOD EVERYTHING")
+	file := loadHTML("ex4.html")
+	links := parseHTML(file)
+	for _, l := range links {
+		fmt.Printf("Href: %s \n", l.Href)
+		fmt.Printf("Text: %s \n", l.Text)
+	}
 }
